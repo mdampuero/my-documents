c6b51a9f4a844be3fa5026e41f46210235a1b1f1
Mauricio Ampuero <mdampuero@gmail.com>
2025-08-08 16:51:46 +0200

    Initial Commit

---
+++ my-documents/my_documentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_documentsApp.swift
+//  my-documents
+//
+//  Created by Mauricio Ampuero on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct my_documentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ my-documentsTests/my_documentsTests.swift
@@ -0,0 +1,16 @@
+//
+//  my_documentsTests.swift
+//  my-documentsTests
+//
+//  Created by Mauricio Ampuero on 8/8/25.
+//
+
+import Testing
+
+struct my_documentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
